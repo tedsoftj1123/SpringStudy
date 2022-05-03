b8bd6caeada317e2d47a5dad2c7985f479f84bda
이하성 <tedsoftj1123@gmail.com>
2022-05-03 16:54:32 +0900

    ADD::GetControllers

---
+++ Study/src/main/java/com/example/study/controller/GetController.java
@@ -0,0 +1,34 @@
+package com.example.study.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+//이제 이 클래스는 컨트롤러일을 한다.
+@RestController
+@RequestMapping("/aaa")//localhost:8080/aaa 로 오는 요청을 처리한다.
+public class GetController {
+    @GetMapping(value = "/name")//aaa/name 으로 오는 요청을 처리한다.
+    public String getName() {
+        return "ted";
+    }
+
+    @GetMapping(value = "/variable/{variable}")// 유저가 입력한 경로 자체를 변수로 저장한다.
+    public String getVal(@PathVariable String variable) {
+        return variable;
+    }
+
+    @GetMapping(value = "/variable/{variable}")// 원래 요기랑 아래 받는 변수 이름이 같아야되는데 이렇게 하면 안해도 됨
+    public String getVariable2(@PathVariable("variable") String var) {
+        return var;
+    }
+    //사이트 가보면 /뭐시기 뒤에 ?오고 뭐
+    //?name=ted&email=tedsofjt1123@gmail.com
+    //뭐 오는걸 파라미터라고 한다.
+    //이걸 처리해준다.
+    @GetMapping(value = "/request")
+    public String GetRequestParam1(
+            @RequestParam String name,
+            @RequestParam String email,
+            @RequestParam String organization) {
+        return name+" "+email+ " " + organization;
+    }
+}
