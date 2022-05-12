4bdf8eeeeb11906616090a6c7e4d067f9ffbc4cd
이하성 <tedsoftj1123@gmail.com>
2022-05-12 13:02:48 +0900

    EDIT :: Study🪄

--- Java/src/main/java/Main.java
+++ Java/src/main/java/Main.java
@@ -1,5 +1,11 @@
 public class Main {
+    public static String delimiter = "";
     public static void main(String[] args) {
-        
+        Student s1 = new Student();
+        s1.age = "17";
+        s1.name = "ted";
+        System.out.println("");
     }
+
+
 }
