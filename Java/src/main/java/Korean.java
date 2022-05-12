6bb372d9a23c932a79ec54fcf386432249c85433
이하성 <tedsoftj1123@gmail.com>
2022-05-12 20:00:26 +0900

    EDIT :: Study

---
+++ Java/src/main/java/Korean.java
@@ -0,0 +1,10 @@
+public class Korean {
+    String nation = "한국";
+    String name;
+    String ssn;
+
+    public Korean(String n, String s) {
+        name = n;
+        ssn = s;
+    }
+}
--- Java/src/main/java/Main.java
+++ Java/src/main/java/Main.java
@@ -1,10 +1,13 @@
 public class Main {
     public static String delimiter = "";
     public static void main(String[] args) {
-        Student s1 = new Student();
-        s1.age = "17";
-        s1.name = "ted";
-        System.out.println("");
+        Korean k1 = new Korean("이하성", "121424213");
+        System.out.println("k1.name : " + k1.name);
+        System.out.println("k1.ssn : " + k1.ssn);
+
+        Korean k2 = new Korean("감자바", "132412414");
+        System.out.println("k2.name : " + k2.name);
+        System.out.println("k2.ssn : " + k2.ssn);
     }
 
 
