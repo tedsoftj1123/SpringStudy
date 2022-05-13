575c007294eab7951b60501954821883c415d902
이하성 <tedsoftj1123@gmail.com>
2022-05-13 19:15:05 +0900

    ADD :: ➕Instance practice, Class Priactice

--- Java/src/main/java/AccountingApp.java
+++ Java/src/main/java/AccountingApp.java
@@ -1,19 +1,29 @@
 class Accounting{
-    public static double valueOfSupply;
+    public double valueOfSupply;
     public static double vatRate = 0.1;
-    public static double getVAT() {
+    public double getVAT() {
         return valueOfSupply * vatRate;
     }
-    public static double getTotal() {
+    public double getTotal() {
         return valueOfSupply + getVAT();
     }
 }
 public class AccountingApp {
     public static void main(String[] args) {
-        Accounting.valueOfSupply = 10000.0;
+        Accounting a1 = new Accounting();
+        a1.valueOfSupply = 10000.0;
+        Accounting a2 = new Accounting();
+        a2.valueOfSupply = 2000.0;
+
+        System.out.println(a1.getVAT());
+        System.out.println(a2.getVAT());
+
+        System.out.println(a1.getTotal());
+        System.out.println(a2.getTotal());
+        /*Accounting.valueOfSupply = 10000.0;
         System.out.println("Value of supply : " + Accounting.valueOfSupply);
         System.out.println("VAT : " + Accounting.getVAT());
-        System.out.println("Total : " + Accounting.getTotal());
+        System.out.println("Total : " + Accounting.getTotal());*/
 
     }
 }
