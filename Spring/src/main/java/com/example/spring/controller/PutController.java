f03223d54efbc4254f3651d0642441f81ee191b0
이하성 <tedsoftj1123@gmail.com>
2022-05-17 13:50:40 +0900

    ADD::Put controller➕

---
+++ Spring/src/main/java/com/example/spring/controller/PutController.java
@@ -0,0 +1,39 @@
+package com.example.spring.controller;
+
+import com.example.spring.DTO.MemberDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class PutController {
+    @PutMapping(value = "/putapi")
+    public String putMethod(){return "HelloWorld!";}
+
+    @PutMapping(value = "/putapi2")
+    public String postMember(@RequestBody Map<String, Object> postData) {
+        StringBuilder sb3 = new StringBuilder();
+
+        postData.entrySet().forEach(map -> {
+            sb3.append(map.getKey()+" : "+map.getValue()+"\n");
+        });
+        return sb3.toString();
+    }
+
+    @PutMapping(value = "/putapi3")
+    public String postMemberDto1(@RequestBody MemberDTO memberDTO){return memberDTO.toString();}
+
+    @PutMapping(value = "/putapi4")
+    public MemberDTO postMemberDto2(@RequestBody MemberDTO memberDTO){
+        return memberDTO;
+    }
+
+    @PutMapping(value = "/putapi5")
+    public ResponseEntity<MemberDTO> postMemberDto3(@RequestBody MemberDTO memberDTO) {
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(memberDTO);
+    }
+}
