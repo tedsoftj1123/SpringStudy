f86ea1dde4adec2f2b70fb69b377f52739fbb9f7
이하성 <tedsoftj1123@gmail.com>
2022-05-21 23:55:51 +0900

    ADD::static, singleton

---
+++ Java/src/main/java/singletonExample.java
@@ -0,0 +1,6 @@
+public class singletonExample {
+    public static void main(String[] args) {
+        Singleton obj1 = Singleton.getInstance();
+        Singleton obj2 = Singleton.getInstance();
+    }
+}
