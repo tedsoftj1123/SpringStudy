0a398c2fd6cfbc0df88762b26b848b87c5a7c9c7
이하성 <tedsoftj1123@gmail.com>
2022-05-21 21:43:22 +0900

    ADD::UserRepositoryTestCase➕

--- Spring2/src/main/java/com/example/spring2/repository/MemoryMemberRepository.java
+++ Spring2/src/main/java/com/example/spring2/repository/MemoryMemberRepository.java
@@ -11,8 +11,8 @@ public class MemoryMemberRepository implements MemberRepository{
 
     @Override
     public Member save(Member member) {
-        member.setId(sequence);
-        store.put(member.getId(), member)
+        member.setId(++sequence);
+        store.put(member.getId(), member);
         return member;
     }
 
@@ -31,5 +31,9 @@ public class MemoryMemberRepository implements MemberRepository{
     @Override
     public List<Member> findAll() {
         return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
     }
 }
