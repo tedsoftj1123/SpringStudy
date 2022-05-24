ab0af3d1743de3375c4e8f2a16a018bfa0ce368d
이하성 <tedsoftj1123@gmail.com>
2022-05-24 20:23:21 +0900

    ADD::member management sys➕
    using mvc pattern

--- Spring2/src/main/java/com/example/spring2/controller/MemberController.java
+++ Spring2/src/main/java/com/example/spring2/controller/MemberController.java
@@ -1,8 +1,14 @@
 package com.example.spring2.controller;
 
+import com.example.spring2.domain.Member;
 import com.example.spring2.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 /*
  스프링 부트가 시작되면 @Componont 라는 어노테이션이 있는 클래스를 자동이로
@@ -17,5 +23,26 @@ public class MemberController {
     @Autowired // memberservice로 의존관계 설정
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
---
+++ Spring2/src/main/java/com/example/spring2/controller/MemberForm.java
@@ -0,0 +1,13 @@
+package com.example.spring2.controller;
+
+public class MemberForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
