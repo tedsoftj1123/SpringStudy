5f9f7e135b8f2936165e66623698ffa0650bcf94
이하성 <tedsoftj1123@gmail.com>
2022-06-04 23:43:40 +0900

    ADD :: SpringDataJpa➕

--- Spring2/src/main/java/com/example/spring2/SpringConfig.java
+++ Spring2/src/main/java/com/example/spring2/SpringConfig.java
@@ -1,7 +1,5 @@
 package com.example.spring2;
 
-import com.example.spring2.repository.JdbcMemberRepository;
-import com.example.spring2.repository.JdbcTemplateMemberRepository;
 import com.example.spring2.repository.JpaMemberRepository;
 import com.example.spring2.repository.MemberRepository;
 import com.example.spring2.service.MemberService;
@@ -9,25 +7,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
-import javax.xml.crypto.Data;
-
 @Configuration
 public class SpringConfig {
 
-    private EntityManager em;
+    private final MemberRepository memberRepository;
+
     @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
-    }
-    @Bean
-    public MemberService memberService() {
-        return new MemberService(memberRepository());
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
+    }
+
+    //SpringDataJpa는 JpaRepository를 상속받으면 알아서 구현체를 만들고 bean에 추가해준다.
+   /* @Bean
     public MemberRepository memberRepository() {
         return new JpaMemberRepository(em);
-    }
+    }*/
 }
