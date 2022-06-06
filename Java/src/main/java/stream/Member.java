eb12e5f98b01393e3cc56a9cfc9f4a810a9385f5
이하성 <tedsoftj1123@gmail.com>
2022-06-06 19:40:58 +0900

    ADD::Java Stream➕

--- Java/src/main/java/stream/Member.java
+++ Java/src/main/java/stream/Member.java
@@ -1,2 +1,24 @@
-package stream;public class Member {
+package stream;
+
+public class Member {
+    public static int MALE = 0;
+    public static int FEMALE = 1;
+
+    private String name;
+    private int sex;
+    private int age;
+
+    public int getSex() {
+        return sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Member(String name, int sex, int age) {
+        this.name = name;
+        this.sex = sex;
+        this.age = age;
+    }
 }
--- Java/src/main/java/stream/StreamExapmle.java
+++ Java/src/main/java/stream/StreamExapmle.java
@@ -1,2 +1,23 @@
-package stream;public class StreamExapmle {
+package stream;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamExapmle {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("a", "c", "b", "d");
+
+
+        //Iterator 사용
+        Iterator<String> iterator = list.iterator();
+        while(iterator.hasNext()) {
+            String alp = iterator.next();
+            System.out.println(alp);
+        }
+
+        //String 사용
+        Stream<String> stream = list.stream();
+        stream.forEach(name -> System.out.println(name));
+    }
 }
