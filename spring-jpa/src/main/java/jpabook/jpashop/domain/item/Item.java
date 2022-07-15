60cbf7d63af037a33157420d63182dcb90d94f31
이하성 <tedsoftj1123@gmail.com>
2022-07-15 23:58:10 +0900

    [ADD] 엔티티 설정

---
+++ spring-jpa/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -0,0 +1,16 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter @Setter
+public abstract class Item {
+    @Id @GeneratedValue
+    @Column(name = "order_item_id")
+}
