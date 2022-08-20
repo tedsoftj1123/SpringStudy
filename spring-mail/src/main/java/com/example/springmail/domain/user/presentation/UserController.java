c06b8b563fe2e6a157acad5bcb8e8f92db88ad0c
이하성 <tedsoftj1123@gmail.com>
2022-08-20 23:11:44 +0900

    🪛 :: 회원가입

--- spring-mail/src/main/java/com/example/springmail/domain/user/presentation/UserController.java
+++ spring-mail/src/main/java/com/example/springmail/domain/user/presentation/UserController.java
@@ -1,17 +1,25 @@
 package com.example.springmail.domain.user.presentation;
 
 import com.example.springmail.domain.user.presentation.dto.SendEmailRequest;
+import com.example.springmail.domain.user.presentation.dto.SignUpRequest;
 import com.example.springmail.domain.user.service.SendAuthCodeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
+@RequestMapping("/user")
 @RestController
 public class UserController {
 
     private final SendAuthCodeService sendAuthCodeService;
+
+    @PostMapping("/auth")
+    public void signUp(@RequestBody SignUpRequest request) {
+
+    }
 
     @PostMapping("/email/send")
     public void sendEmail(@RequestBody SendEmailRequest request) {
---
+++ spring-mail/src/main/java/com/example/springmail/domain/user/service/UserSignUpService.java
@@ -0,0 +1,17 @@
+package com.example.springmail.domain.user.service;
+
+import com.example.springmail.domain.user.facade.UserFacade;
+import com.example.springmail.domain.user.presentation.dto.SignUpRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserSignUpService {
+
+    private final UserFacade userFacade;
+
+    public void execute(SignUpRequest request) {
+        userFacade.validateDuplicateMember(request.getEmail());
+    }
+}
