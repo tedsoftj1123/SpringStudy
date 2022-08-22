a2d3342d42bba9cc5aa1940b52c1307542da9c4a
이하성 <tedsoftj1123@gmail.com>
2022-08-22 17:38:03 +0900

    🪛 :: setter추가

--- spring-mail/src/main/java/com/example/springmail/domain/user/domain/AuthCode.java
+++ spring-mail/src/main/java/com/example/springmail/domain/user/domain/AuthCode.java
@@ -1,21 +1,21 @@
 package com.example.springmail.domain.user.domain;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 
 @Entity
+@Getter
+@Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class AuthCode {
-
     @Id
     @Column(name = "auth_code_id")
+    private String email;
+
     private String authCode;
 
-    private String email;
+    private boolean verified;
 }
