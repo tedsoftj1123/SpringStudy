3308439a9b97a0c20bf6ada0282b6c4f442bed74
이하성 <tedsoftj1123@gmail.com>
2022-08-26 22:08:07 +0900

    🪛 :: 도메인 설계

--- Board/src/main/java/com/example/board/domain/user/domain/Member.java
+++ Board/src/main/java/com/example/board/domain/user/domain/Member.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -16,6 +17,9 @@ public class Member {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "member_email", unique = true, length = 25)
+    private String email;
+
     @Column(name = "member_nickname", length = 10)
     private String nickname;
 
@@ -24,12 +28,12 @@ public class Member {
 
 
     @OneToMany
-
-    private List<Board> boards;
+    private List<Board> boards = new ArrayList<>();
 
 
     @Builder
-    public Member(String nickname, String password) {
+    public Member(String nickname, String password, String email) {
+        this.email = email;
         this.nickname = nickname;
         this.password = password;
     }
--- Board/src/main/java/com/example/board/domain/user/domain/MemberRepository.java
+++ Board/src/main/java/com/example/board/domain/user/domain/MemberRepository.java
@@ -3,5 +3,4 @@ package com.example.board.domain.user.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Member
 }
