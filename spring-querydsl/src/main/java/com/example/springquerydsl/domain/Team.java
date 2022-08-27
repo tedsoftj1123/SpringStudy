2177272af27fb9ec601d75213333577300c5d679
이하성 <tedsoftj1123@gmail.com>
2022-08-27 14:56:50 +0900

    🪛 :: 테스트용 엔티티생성

--- spring-querydsl/src/main/java/com/example/springquerydsl/domain/Team.java
+++ spring-querydsl/src/main/java/com/example/springquerydsl/domain/Team.java
@@ -1,2 +1,23 @@
-package com.example.springquerydsl.domain;public class Team {
+package com.example.springquerydsl.domain;
+
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Team {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String teamName;
+
+    @OneToMany
+    private List<Member> members = new ArrayList<>();
 }
