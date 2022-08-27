5d8eb4f4b03ec0e14239dbd4b0a412738e807179
이하성 <tedsoftj1123@gmail.com>
2022-08-27 14:55:57 +0900

    🔧 :: InitProject

---
+++ spring-querydsl/src/main/java/com/example/springquerydsl/domain/Team.java
@@ -0,0 +1,2 @@
+package com.example.springquerydsl.domain;public class Team {
+}
---
+++ spring-querydsl/src/test/java/com/example/springquerydsl/SpringQuerydslApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springquerydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringQuerydslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
