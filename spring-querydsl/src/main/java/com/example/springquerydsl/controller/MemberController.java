3a5ba6652aeadd93f32e6f74cbf2eaf3f3d8bb0c
이하성 <tedsoftj1123@gmail.com>
2022-09-01 18:28:34 +0900

    .

--- spring-querydsl/src/main/java/com/example/springquerydsl/controller/MemberController.java
+++ spring-querydsl/src/main/java/com/example/springquerydsl/controller/MemberController.java
@@ -2,18 +2,29 @@ package com.example.springquerydsl.controller;
 
 import com.example.springquerydsl.domain.Member;
 import com.example.springquerydsl.service.MemberSignUpService;
+import com.example.springquerydsl.service.QueryFindUserBetweenService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberSignUpService service;
+    private final QueryFindUserBetweenService queryFindUserBetweenService;
 
     @GetMapping("/{id}")
-    public Member test(@PathVariable Long id) {
+    public Member test(@PathVariable Long id)  {
         return service.search(id);
+    }
+
+    @GetMapping
+    public List<Member> serchUser(@RequestParam int a, @RequestParam int b) {
+        return queryFindUserBetweenService.execute(a, b);
     }
 }
--- spring-querydsl/src/main/java/com/example/springquerydsl/service/QueryFindUserBetweenService.java
+++ spring-querydsl/src/main/java/com/example/springquerydsl/service/QueryFindUserBetweenService.java
@@ -5,12 +5,14 @@ import com.example.springquerydsl.domain.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
-public class MemberSignUpService {
+public class QueryFindUserBetweenService {
     private final MemberRepository memberRepository;
 
-    public Member search(Long id) {
-        return memberRepository.findMemberById(id);
+    public List<Member> execute(int a, int b) {
+        return memberRepository.findAllMemberWithSomething(a, b);
     }
 }
