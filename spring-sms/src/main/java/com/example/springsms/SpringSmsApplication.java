cb746917f96653758f3187a1e7074cfde8182362
이하성 <tedsoftj1123@gmail.com>
2022-09-06 17:08:30 +0900

    sms 프로젝트 생성

---
+++ spring-sms/src/main/java/com/example/springsms/SpringSmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSmsApplication.class, args);
+    }
+
+}
