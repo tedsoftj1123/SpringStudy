6fecf8f235b78b9e2effe2cbc9cd01b487f49be6
이하성 <tedsoftj1123@gmail.com>
2022-09-09 20:28:34 +0900

    🪛 :: 회원가입 도메인

--- spring-sms/src/main/java/com/example/springsms/domain/user/controller/UserController.java
+++ spring-sms/src/main/java/com/example/springsms/domain/user/controller/UserController.java
@@ -1,11 +1,27 @@
 package com.example.springsms.domain.user.controller;
 
+import com.example.springsms.domain.user.controller.dto.SignupRequest;
+import com.example.springsms.domain.user.controller.dto.UserSendMailRequest;
 import com.example.springsms.domain.user.service.SendAuthCodeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserController {
     private final SendAuthCodeService sendAuthCodeService;
+
+    @PostMapping("/send")
+    public void sendUserAuthCode(@RequestBody UserSendMailRequest request) {
+        sendAuthCodeService.execute(request);
+    }
+
+    @PostMapping("/sign-up")
+    public void signUp(@RequestBody SignupRequest request) {
+
+    }
 }
