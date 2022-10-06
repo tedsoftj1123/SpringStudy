585105c8e41c6f5ada65579d121accde36dc906f
이하성 <tedsoftj1123@gmail.com>
2022-10-06 19:03:47 +0900

    ⚙️ :: S3 Config

---
+++ spring-s3/src/main/java/com/example/springs3/SpringS3Application.java
@@ -0,0 +1,15 @@
+package com.example.springs3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@SpringBootApplication
+@ConfigurationPropertiesScan
+public class SpringS3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringS3Application.class, args);
+    }
+
+}
