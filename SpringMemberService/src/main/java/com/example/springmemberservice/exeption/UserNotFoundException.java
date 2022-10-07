5c6aeab5c21db84fe457c012d3dab37d2ea7faad
이하성 <tedsoftj1123@gmail.com>
2022-10-07 19:40:18 +0900

    폴더 정리

--- SpringMemberService/src/main/java/com/example/springmemberservice/exeption/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.springmemberservice.exeption;
-
-import com.example.springmemberservice.error.ErrorCode;
-
-public class UserNotFoundException extends BusinessException{
-    public UserNotFoundException(){
-        super(ErrorCode.USER_NOT_FOUND);
-    }
-}
--- SpringMemberService/src/main/java/com/example/springmemberservice/exeption/WrongPasswordException.java
+++
@@ -1,9 +0,0 @@
-package com.example.springmemberservice.exeption;
-
-import com.example.springmemberservice.error.ErrorCode;
-
-public class WrongPasswordException extends BusinessException{
-    public WrongPasswordException() {
-        super(ErrorCode.WRONG_PASSWORD);
-    }
-}
