8c1514b886d50ab6ee29280bf8e72e83ce61ea77
이하성 <tedsoftj1123@gmail.com>
2022-10-14 22:49:41 +0900

    📑 :: socket event로 채팅

--- spring-websocket/src/main/java/com/example/springwebsocket/controller/ChatController.java
+++ spring-websocket/src/main/java/com/example/springwebsocket/controller/ChatController.java
@@ -1,9 +1,20 @@
 package com.example.springwebsocket.controller;
 
+import com.corundumstudio.socketio.SocketIOClient;
+import com.corundumstudio.socketio.annotation.OnEvent;
+import com.example.springwebsocket.controller.dto.MessageRequest;
+import com.example.springwebsocket.service.SendMessageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class ChatController {
+
+    private final SendMessageService sendMessageService;
+    @OnEvent("send_message")
+    public void sendMsg(SocketIOClient socketIOClient, @RequestBody MessageRequest request) {
+        sendMessageService.sendMessage(socketIOClient, request);
+    }
 }
