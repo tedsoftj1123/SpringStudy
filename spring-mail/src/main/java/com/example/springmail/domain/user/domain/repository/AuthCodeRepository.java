3164e1bb237740520c1052e894a94966dcab9d19
이하성 <tedsoftj1123@gmail.com>
2022-10-15 23:45:53 +0900

    레포정리

--- spring-mail/src/main/java/com/example/springmail/domain/user/domain/repository/AuthCodeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.springmail.domain.user.domain.repository;
-
-import com.example.springmail.domain.user.domain.AuthCode;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthCodeRepository extends JpaRepository<AuthCode, String> {
-    boolean existsByEmail(String mail);
-}
