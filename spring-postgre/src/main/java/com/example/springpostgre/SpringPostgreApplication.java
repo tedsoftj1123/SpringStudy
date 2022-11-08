15db54fbe436d7280c3307af2bb9fc9183e18a59
이하성 <tedsoftj1123@gmail.com>
2022-11-08 23:10:22 +0900

    ⚙️ :: postgre 프로젝트

---
+++ spring-postgre/src/main/java/com/example/springpostgre/SpringPostgreApplication.java
@@ -0,0 +1,13 @@
+package com.example.springpostgre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPostgreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPostgreApplication.class, args);
+    }
+
+}
---
+++ spring-postgre/src/test/java/com/example/springpostgre/SpringPostgreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springpostgre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPostgreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
