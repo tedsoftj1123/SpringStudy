ec45765ee84f780a1e3adaf17166248f8d411729
이하성 <tedsoftj1123@gmail.com>
2022-12-07 22:43:00 +0900

    회원가입

---
+++ spring-kotlin/src/main/kotlin/com/example/springkotlin/domain/user/SignupRequest.kt
@@ -0,0 +1,6 @@
+package com.example.springkotlin.domain.user
+
+data class SignupRequest(
+    val accountId:String,
+    val password:String
+)
--- spring-kotlin/src/main/kotlin/com/example/springkotlin/domain/user/UserRepository.kt
+++ spring-kotlin/src/main/kotlin/com/example/springkotlin/domain/user/UserRepository.kt
@@ -1,6 +1,8 @@
-package com.example.springkotlin.domain.userr
+package com.example.springkotlin.domain.user
 
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface UserRepository : JpaRepository<User, Long>{
+    
+
 }
