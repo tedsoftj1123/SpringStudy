d64a6429a77703b755b786a7ce0b647f8c4a825f
이하성 <tedsoftj1123@gmail.com>
2023-01-03 14:30:14 +0900

    리뉴얼

--- spring-querydsl/src/main/java/com/example/springquerydsl/SpringQuerydslApplication.java
+++ spring-querydsl/src/main/java/com/example/springquerydsl/SpringQuerydslApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringQuerydslApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringQuerydslApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringQuerydslApplication.class, args);
+    }
 
 }
--- spring-querydsl/src/test/java/com/example/springquerydsl/SpringQuerydslApplicationTests.java
+++ spring-querydsl/src/test/java/com/example/springquerydsl/SpringQuerydslApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringQuerydslApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
