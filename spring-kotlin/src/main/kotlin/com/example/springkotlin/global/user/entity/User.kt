68b462a5867e13620a2872cd066efeeeaff3f4a7
이하성 <tedsoftj1123@gmail.com>
2023-01-06 20:21:15 +0900

    user

--- spring-kotlin/src/main/kotlin/com/example/springkotlin/global/user/entity/User.kt
+++ spring-kotlin/src/main/kotlin/com/example/springkotlin/global/user/entity/User.kt
@@ -1,15 +1,13 @@
 package com.example.springkotlin.global.user.entity
 
 import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import java.time.LocalDateTime
 import java.util.UUID
 
 @Entity
 class User(
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
     val userId: UUID = UUID.randomUUID(),
 
     val accountId: String,
--- spring-kotlin/src/main/kotlin/com/example/springkotlin/global/user/entity/repository/UserRepository.kt
+++ spring-kotlin/src/main/kotlin/com/example/springkotlin/global/user/entity/repository/UserRepository.kt
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 import java.util.UUID
 
 interface UserRepository : JpaRepository<User, UUID> {
-    fun findByAccountId(accountId: String)
+    fun findByAccountId(accountId: String): User?
 }
