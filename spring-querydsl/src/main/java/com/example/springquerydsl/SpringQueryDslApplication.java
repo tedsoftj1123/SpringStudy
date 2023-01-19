928a9ce2fc072916e6149583702a3ad06d9c4176
이하성 <tedsoftj1123@gmail.com>
2023-01-19 13:18:38 +0900

    ✅ :: to java^^

---
+++ spring-querydsl/src/main/java/com/example/springquerydsl/SpringQueryDslApplication.java
@@ -0,0 +1,13 @@
+package com.example.springquerydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringQueryDslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringQueryDslApplication.class, args);
+	}
+
+}
---
+++ spring-querydsl/src/test/java/com/example/springquerydsl/SpringQueryDslApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springquerydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringQueryDslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
