06826a31db526772f9c32d60e604523bb70a610d
이하성 <tedsoftj1123@gmail.com>
2023-01-19 12:34:08 +0900

    ✅ :: to kotlin project

---
+++ spring-querydsl/src/main/kotlin/com/example/springquerydsl/SpringQuerydslApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springquerydsl
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringQuerydslApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringQuerydslApplication>(*args)
+}
