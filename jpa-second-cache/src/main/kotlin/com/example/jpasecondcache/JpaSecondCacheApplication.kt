a24766bfdfe0b60564742a46378bc69e0e720958
이하성 <tedsoftj1123@gmail.com>
2023-05-08 15:15:36 +0900

    jpa2차

---
+++ jpa-second-cache/src/main/kotlin/com/example/jpasecondcache/JpaSecondCacheApplication.kt
@@ -0,0 +1,11 @@
+package com.example.jpasecondcache
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JpaSecondCacheApplication
+
+fun main(args: Array<String>) {
+    runApplication<JpaSecondCacheApplication>(*args)
+}
---
+++ jpa-second-cache/src/test/kotlin/com/example/jpasecondcache/JpaSecondCacheApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.jpasecondcache
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JpaSecondCacheApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
